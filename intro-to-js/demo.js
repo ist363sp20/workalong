b99fdfeda9cfd503a9335ec4aa43f56740976b24
Nick <ndlyga@syr.edu>
2020-02-18 18:09:21 -0500

    intro to js

---
+++ intro-to-js/demo.js
@@ -0,0 +1,14 @@
+let myname = "Nick"
+let myage = 20;
+
+myage = myage + 20;
+
+myage++;
+
+var clicked = 0;
+document.getElementById("clicker").addEventListener("click", function() {
+    clicked++;
+    document.getElementById("count").innerHTML = clicked;
+})
+
+if 
---
+++ intro-to-js/notes.md
@@ -0,0 +1,12 @@
+## Notes
+
+Demos:
+
+- inlining js in script tag
+- Adding js link to html
+
+- creating variables
+- doing simple math
+
+- operators
+
