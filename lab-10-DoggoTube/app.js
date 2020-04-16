e91e52a26ceb9c140e9fe2a9f403eea714823192
Nick Lyga <ndlyga@syr.edu>
2020-04-16 13:16:26 -0400

    Update app.js

--- lab-10-DoggoTube/app.js
+++ lab-10-DoggoTube/app.js
@@ -1,7 +1,7 @@
 // You will have 4 Total Components one for each section
 // You will also need and API from your google account!
 
-const apiKey = 'AIzaSyCqOC0D_QtQbJhJdKUQOJ-vcqEiObkK7KQ';
+const apiKey = '';
 const url = "https://www.googleapis.com/youtube/v3/search";
 
 
